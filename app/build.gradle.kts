a3636098362289364963bc7e8a9560db9f6b82a7
Juan Gaines <juan.gaines@platzi.com>
2025-03-18 15:56:00 -0500

    map config key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
     alias(libs.plugins.dagger.hilt)
     alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.mapsplatform.secrets.plugin)
+
 }
 
 android {
