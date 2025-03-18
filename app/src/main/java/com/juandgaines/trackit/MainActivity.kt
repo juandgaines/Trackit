336fb68b178a0e10160819434f1b383987be748f
Juan Gaines <juan.gaines@platzi.com>
2025-03-18 18:11:40 -0500

    feat: flows and Timer.kt

--- app/src/main/java/com/juandgaines/trackit/MainActivity.kt
+++ app/src/main/java/com/juandgaines/trackit/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.juandgaines.trackit
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,22 +9,102 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.google.maps.android.compose.GoogleMap
+import com.juandgaines.trackit.domain.timer.Timer
 import com.juandgaines.trackit.presentation.maps.MapSection
 import com.juandgaines.trackit.ui.theme.TrackitTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.combineTransform
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.reduce
+import kotlinx.coroutines.flow.scan
+import kotlinx.coroutines.flow.zip
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.time.delay
 import kotlinx.serialization.Serializable
+import kotlin.time.Duration
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val stateChannel = MutableStateFlow(false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        lifecycleScope.launch {
+            var counter:Duration = Duration.ZERO
+
+            Timer.timeAndEmits().collect{
+                counter+=it
+                Log.d("FlowTimer", "Time interval ${counter.inWholeSeconds} s")
+            }
+        }
+
+        Timer.randomFlow().onEach {
+            Log.d("FlowRandom", "Random number $it")
+        }.launchIn(lifecycleScope)
+
+        Timer.timeAndEmits()
+            .scan(Duration.ZERO){ acc, value ->
+                acc + value
+            }
+            .combine(
+                Timer.randomFlow()
+            ){ time, random ->
+                time to random
+            }.onEach {
+                Log.d("FlowWithCombine", "Time interval ${it.first.inWholeSeconds} s, Random number ${it.second}")
+            }.launchIn(lifecycleScope)
+
+
+        Timer.timeAndEmits()
+            .scan(Duration.ZERO){ acc, value ->
+                acc + value
+            }
+            .zip(
+                Timer.randomFlow()
+            ){ time, random ->
+                time to random
+            }.onEach {
+                Log.d("FlowWithZip", "Time interval ${it.first.inWholeSeconds} s, Random number ${it.second}")
+            }.launchIn(lifecycleScope)
+
+        stateChannel.flatMapLatest{ channel->
+            if (channel){
+                Timer.timeAndEmits().scan(Duration.ZERO){acc, value ->
+                    acc + value
+                }
+            }
+            else{
+                Timer.randomFlow()
+            }
+
+        }.onEach {
+            if(stateChannel.value)
+                Log.d("FlowWithFlatMapLatest", "Time interval ${(it as Duration).inWholeSeconds} s")
+            else
+                Log.d("FlowWithFlatMapLatest", "Random number ${(it as Int)} s")
+
+        }.launchIn(lifecycleScope)
+
+        lifecycleScope.launch {
+            kotlinx.coroutines.delay(10000L)
+            stateChannel.value = true
+        }
+
         setContent {
             val navController = rememberNavController()
             TrackitTheme {
---
+++ app/src/main/java/com/juandgaines/trackit/domain/timer/Timer.kt
@@ -0,0 +1,31 @@
+package com.juandgaines.trackit.domain.timer
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+
+object Timer {
+    fun timeAndEmits(): Flow<Duration> {
+        return flow {
+            var lastEmitTime= System.currentTimeMillis()
+            while(true){
+                delay(200L)
+                val currentTime = System.currentTimeMillis()
+                val elapsedTime = currentTime - lastEmitTime
+                emit(elapsedTime.milliseconds)
+                lastEmitTime = currentTime
+            }
+        }
+    }
+
+    fun randomFlow(): Flow<Int> {
+        return flow {
+            while(true){
+                delay(1000L)
+                emit((0..100).random())
+            }
+        }
+    }
+}
