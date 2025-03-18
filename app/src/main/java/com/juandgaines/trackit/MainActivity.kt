334be94223f7da5c0221d18995d0057206542d1a
Juan Gaines <juan.gaines@platzi.com>
2025-03-18 16:06:45 -0500

    feat:di

--- app/src/main/java/com/juandgaines/trackit/MainActivity.kt
+++ app/src/main/java/com/juandgaines/trackit/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : ComponentActivity() {
                     ){
                         composable<MapScreenDes> (){
 
+
                         }
 
                         composable<CameraScreenDes> {
