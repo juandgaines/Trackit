4da80de7a17279778db8335c8acf61908a2e681e
Juan Gaines <juan.gaines@platzi.com>
2025-03-20 14:16:35 -0500

    feat: location files

--- app/src/main/java/com/juandgaines/trackit/data/di/DataModule.kt
+++ app/src/main/java/com/juandgaines/trackit/data/di/DataModule.kt
@@ -3,11 +3,13 @@ package com.juandgaines.trackit.data.di
 import android.content.Context
 import com.juandgaines.trackit.data.AndroidLocationObserver
 import com.juandgaines.trackit.domain.location.LocationObserver
+import com.juandgaines.trackit.domain.location.LocationTracker
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
 import javax.inject.Singleton
 
 @Module
@@ -21,5 +23,17 @@ class DataModule {
         context: Context
     ): LocationObserver {
         return AndroidLocationObserver(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideLocationTracker(
+        locationObserver: LocationObserver,
+        coroutineScope: CoroutineScope
+    ): LocationTracker {
+        return LocationTracker(
+            locationObserver,
+            coroutineScope
+        )
     }
 }
