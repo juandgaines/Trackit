3cd1070f383319969070b835aa88c472799d53bc
Juan Gaines <juan.gaines@platzi.com>
2025-03-20 21:00:07 -0500

    feat: map section

--- app/src/main/java/com/juandgaines/trackit/presentation/maps/MapSection.kt
+++ app/src/main/java/com/juandgaines/trackit/presentation/maps/MapSection.kt
@@ -3,6 +3,8 @@ package com.juandgaines.trackit.presentation.maps
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.LocalActivity
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
@@ -13,6 +15,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,7 +32,10 @@ import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.MapsComposeExperimentalApi
 import com.google.maps.android.compose.MarkerComposable
+import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberUpdatedMarkerState
+import com.juandgaines.trackit.domain.location.Location
+import com.juandgaines.trackit.domain.location.LocationWithTimestamp
 
 val latLngArray =  listOf(
     LatLng(4.6547591408952185, -74.05578687079682),
@@ -39,19 +46,39 @@ val latLngArray =  listOf(
 @OptIn(MapsComposeExperimentalApi::class)
 @Composable
 fun MapSection(
-    modifier:Modifier = Modifier
+    modifier:Modifier = Modifier,
+    currentLocation: Location?,
+    isTrackingFinished: Boolean,
+    selectedLocation: LocationWithTimestamp? = null,
+    locations: List<List<LocationWithTimestamp>>,
 ){
     val activity = LocalActivity.current as ComponentActivity
 
+    val cameraPositionState = rememberCameraPositionState()
     val marker = rememberUpdatedMarkerState()
 
 
+    val markerPositionLat by animateFloatAsState(
+        targetValue = currentLocation?.lat?.toFloat() ?: 0f,
+        animationSpec = tween(durationMillis = 500),
+        label = ""
+    )
 
-    LaunchedEffect(true) {
+    val markerPositionLong by animateFloatAsState(
+        targetValue = currentLocation?.long?.toFloat() ?: 0f,
+        animationSpec = tween(durationMillis = 500),
+        label = ""
+    )
 
-        marker.position = LatLng(
-            4.6547591408952185, -74.05578687079682
-        )
+    val markerPosition = remember(markerPositionLat, markerPositionLong) {
+        LatLng(markerPositionLat.toDouble(), markerPositionLong.toDouble())
+    }
+
+
+    LaunchedEffect(markerPosition, isTrackingFinished) {
+        if(!isTrackingFinished) {
+            marker.position = markerPosition
+        }
     }
 
     GoogleMap(
@@ -86,22 +113,24 @@ fun MapSection(
 
         }
 
-        MarkerComposable (
-            state = marker
-        ){
-            Box(
-                modifier = Modifier
-                    .size(35.dp)
-                    .clip(CircleShape)
-                    .background(MaterialTheme.colorScheme.primary),
-                contentAlignment = Alignment.Center
+        if(!isTrackingFinished && currentLocation != null) {
+            MarkerComposable(
+                state = marker
             ) {
-                Icon(
-                    imageVector = Icons.Default.Camera,
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.onPrimary,
-                    modifier = Modifier.size(20.dp)
-                )
+                Box(
+                    modifier = Modifier
+                        .size(35.dp)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.primary),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Camera,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onPrimary,
+                        modifier = Modifier.size(20.dp)
+                    )
+                }
             }
         }
     }
