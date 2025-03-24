7be692e6f0a77c739608a4b27965615895c792ca
Juan Gaines <juan.gaines@platzi.com>
2025-03-24 12:45:48 -0500

    feat: camera ui and integration.

--- app/src/main/java/com/juandgaines/trackit/TrackitService.kt
+++ app/src/main/java/com/juandgaines/trackit/TrackitService.kt
@@ -1,9 +1,12 @@
 package com.juandgaines.trackit
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.content.getSystemService
 import com.juandgaines.trackit.domain.location.LocationTracker
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
@@ -14,11 +17,22 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import java.util.Locale
 import javax.inject.Inject
+import kotlin.time.Duration
 
 @AndroidEntryPoint
 class TrackitService: Service() {
 
+    private val notificationManager by lazy {
+        getSystemService<NotificationManager>()!!
+    }
+
+    private val baseNotification by lazy {
+        NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(getString(R.string.active_run))
+    }
 
     @Inject
     lateinit var locationTracker: LocationTracker
@@ -37,8 +51,21 @@ class TrackitService: Service() {
     private fun start(){
         if (!_isServiceActive.value){
             _isServiceActive.value = true
+
+            createNotificationChannel()
+
+            val notification = baseNotification
+                .setContentText("00:00:00")
+                .build()
+
+            startForeground(1, notification)
+
             locationTracker.elapsedTime.onEach { elapsedTime ->
-                Log.d("Service", "elapsedTime: $elapsedTime")
+                val notification = baseNotification
+                    .setContentText(elapsedTime.formatted())
+                    .build()
+
+                notificationManager.notify(1, notification)
 
             }.launchIn(serviceScope)
         }
@@ -53,10 +80,31 @@ class TrackitService: Service() {
     }
     override fun onBind(p0: Intent?): IBinder? = null
 
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            getString(R.string.active_run),
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(channel)
+    }
+
+
     companion object {
         private val _isServiceActive = MutableStateFlow(false)
         var isServiceActive = _isServiceActive.asStateFlow()
         const val ACTION_START = "ACTION_START"
         const val ACTION_STOP = "ACTION_STOP"
+        private const val CHANNEL_ID = "tracking_location"
     }
+}
+
+fun Duration.formatted(): String {
+    val totalSeconds = inWholeSeconds
+    val hours = String.format(Locale.US,"%02d", totalSeconds / (60 * 60))
+    val minutes = String.format(Locale.US,"%02d", (totalSeconds % 3600) / 60)
+    val seconds = String.format(Locale.US,"%02d", (totalSeconds % 60))
+
+    return "$hours:$minutes:$seconds"
 }
